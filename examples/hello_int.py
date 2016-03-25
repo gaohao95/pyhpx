55f02432d9186afea7fa10763fcad8e710b64d56
Andreas Kloeckner <inform@tiker.net>
2016-03-25 11:50:39 -0500

    Segfault fixes

--- examples/hello_int.py
+++ examples/hello_int.py
@@ -1,5 +1,7 @@
 from _hpx import lib, ffi
 
+import faulthandler
+faulthandler.enable()
 
 @ffi.callback("void(int, int)")
 def add_int_func(v1, v2):
@@ -12,5 +14,8 @@ action_key = ffi.new("char[]", b"aaa")
 lib.hpx_register_action(lib.HPX_DEFAULT, 0, action_key, add_int, 3,
                         add_int_func, lib.HPX_INT_lvalue, lib.HPX_INT_lvalue)
 lib.hpx_init(ffi.NULL, ffi.NULL)
-lib._hpx_run(add_int, 2, ffi.cast("int", 42), ffi.cast("int", 21))
+num_a = ffi.new("int *", 42)
+num_b = ffi.new("int *", 21)
+print(num_a)
+lib._hpx_run(add_int, 2, num_a, num_b)
 lib.hpx_finalize()
