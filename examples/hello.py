099d9603055b456dd9f6c394ae773617c4ca6e4c
Hao Gao <gaohao95@gmail.com>
2016-04-18 05:51:29 -0500

    User must specify function signiture

--- examples/hello.py
+++ examples/hello.py
@@ -2,15 +2,14 @@ import hpx
 
 
 # Set up the callback function
-def add(a, b, message):
-    print(a + b)
-    print(message)
+def add(a):
+    print(a)
     hpx.exit(0)
 
 # Register action
-add_action = hpx.register_action(add, hpx.DEFAULT, hpx.ATTR_NONE)
+add_action = hpx.register_action(add, hpx.DEFAULT, hpx.ATTR_NONE, [hpx.INT])
 
 # Initialize HPX runtime and run the action
 hpx.init()
-hpx.run(add_action, 5, 6, "Hello World!")
+hpx.run(add_action, 5)
 hpx.finalize()
--- source/hpx.py
+++ source/hpx.py
@@ -1,9 +1,5 @@
 from build._hpx import ffi, lib
 
-# handle_set store the reference to C memory so that it is not garbage collected by Python
-handle_set = set()
-
-
 # Initializes the HPX runtime.
 # This must be called before other HPX functions.
 def init(argv=[]):
@@ -21,17 +17,14 @@ def init(argv=[]):
     if lib.hpx_init(c_argc, c_argv_address) != lib.HPX_SUCCESS:
         raise RuntimeError("hpx_init failed")
 
-
 # Exit the HPX runtime.
 def exit(code):
     lib.hpx_exit(code)
 
 
-def run(action_id, *args, **kwargs):
-    args_handle = ffi.new_handle(args)
-    handle_set.add(args_handle)
-    args_handle_address = ffi.new("void**", args_handle)
-    lib._hpx_run(action_id, 1, args_handle_address)
+def run(action_id, *args):
+    arg = ffi.new("int* ", args[0])
+    lib._hpx_run(action_id, 1, arg)
 
 
 def finalize():
@@ -69,21 +62,61 @@ COALESCED = lib.HPX_COALESCED
 # Action is a compressed action
 COMPRESSED = lib.HPX_COMPRESSED
 
+# Define argument types
+CHAR = lib.HPX_CHAR_lvalue
+SHORT = lib.HPX_SHORT_lvalue
+USHORT = lib.HPX_USHORT_lvalue
+SSHORT = lib.HPX_SSHORT_lvalue
+INT = lib.HPX_INT_lvalue
+UINT = lib.HPX_UINT_lvalue
+SINT = lib.HPX_SINT_lvalue
+LONG = lib.HPX_LONG_lvalue
+ULONG = lib.HPX_ULONG_lvalue
+SLONG = lib.HPX_SLONG_lvalue
+VOID = lib.HPX_VOID_lvalue
+UINT8 = lib.HPX_UINT8_lvalue
+SINT8 = lib.HPX_SINT8_lvalue
+UINT16 = lib.HPX_UINT16_lvalue
+SINT16 = lib.HPX_SINT16_lvalue
+UINT32 = lib.HPX_UINT32_lvalue
+SINT32 = lib.HPX_SINT32_lvalue
+UINT64 = lib.HPX_UINT64_lvalue
+SINT64 = lib.HPX_SINT64_lvalue
+FLOAT = lib.HPX_FLOAT_lvalue
+DOUBLE = lib.HPX_DOUBLE_lvalue
+POINTER = lib.HPX_POINTER_lvalue
+LONGDOUBLE = lib.HPX_LONGDOUBLE_lvalue
+COMPLEX_FLOAT = lib.HPX_COMPLEX_FLOAT_lvalue
+COMPLEX_DOUBLE = lib.HPX_COMPLEX_DOUBLE_lvalue
+COMPLEX_LONGDOUBLE = lib.HPX_COMPLEX_LONGDOUBLE_lvalue
+
+# Define a dictionary to map argument types to C definition
+_c_def_map = {
+    CHAR: "char",
+    SHORT: "short",
+    USHORT: "unsigned short",
+    SSHORT: "signed short",
+    INT: "int", 
+    UINT: "unsigned int"
+}
+
 
 # Helper function to generate a suitable C function from user-defined Python function
-def _generate_hpx_action(user_action):
-    def hpx_action(user_args_handle):
-        user_args = ffi.from_handle(user_args_handle)
-        handle_set.remove(user_args_handle)
-        return user_action(user_args)
-    return ffi.callback("int(void*)")(hpx_action)
+def _generate_hpx_action(user_action, action_arguments):
+    action_arguments_cdef = []
+    for argument in action_arguments:
+        action_arguments_cdef.append(_c_def_map[argument])
+    return ffi.callback("int(" + ",".join(action_arguments_cdef) + ")")(user_action)
 
 
 # Register an HPX action
 # This must be called prior to hpx_init().
-def register_action(action, action_type, action_attribute):
+# @action: a Python function to be registered as a HPX action
+# @action_type, @action_attribute see above
+# @action_arguments a list of argument types
+def register_action(action, action_type, action_attribute, action_arguments):
     action_id = ffi.new("hpx_action_t *")
-    hpx_action = _generate_hpx_action(action)
+    hpx_action = _generate_hpx_action(action, action_arguments)
     lib.hpx_register_action(action_type, action_attribute, b'aaa',
-                            action_id, 2, hpx_action, lib.HPX_POINTER_lvalue)
+                            action_id, len(action_arguments) + 1, hpx_action, *action_arguments)
     return action_id
