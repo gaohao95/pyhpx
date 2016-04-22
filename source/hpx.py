1cd7e7e954d22fcbb2c51a3a4d0a8761b950bed5
Hao Gao <gaohao95@gmail.com>
2016-04-21 20:35:26 -0500

    support multiple arguments in hpx.run

--- source/hpx.py
+++ source/hpx.py
@@ -1,33 +1,56 @@
 from build._hpx import ffi, lib
 
 
-# Initializes the HPX runtime.
-# This must be called before other HPX functions.
-def init(argv=[]):
-    if len(argv) == 0:
-        c_argc = ffi.NULL
-        c_argv_address = ffi.NULL
-    else:
-        c_argc = ffi.new("int *", len(argv))
-        c_argv = ffi.new("char* []", len(argv))
-        c_argv_obj = [] # store the reference to make sure it is not garbage collected
-        for i in range(len(argv)):
-            c_argv_obj.append(ffi.new("char[]", argv[i].encode('ascii')))
-            c_argv[i] = c_argv_obj[i]
-        c_argv_address = ffi.new("char ***", c_argv)
-    if lib.hpx_init(c_argc, c_argv_address) != lib.HPX_SUCCESS:
-        raise RuntimeError("hpx_init failed")
+# Define argument types
+CHAR = lib.HPX_CHAR_lvalue
+SHORT = lib.HPX_SHORT_lvalue
+USHORT = lib.HPX_USHORT_lvalue
+SSHORT = lib.HPX_SSHORT_lvalue
+INT = lib.HPX_INT_lvalue
+UINT = lib.HPX_UINT_lvalue
+SINT = lib.HPX_SINT_lvalue
+LONG = lib.HPX_LONG_lvalue
+ULONG = lib.HPX_ULONG_lvalue
+SLONG = lib.HPX_SLONG_lvalue
+VOID = lib.HPX_VOID_lvalue
+UINT8 = lib.HPX_UINT8_lvalue
+SINT8 = lib.HPX_SINT8_lvalue
+UINT16 = lib.HPX_UINT16_lvalue
+SINT16 = lib.HPX_SINT16_lvalue
+UINT32 = lib.HPX_UINT32_lvalue
+SINT32 = lib.HPX_SINT32_lvalue
+UINT64 = lib.HPX_UINT64_lvalue
+SINT64 = lib.HPX_SINT64_lvalue
+FLOAT = lib.HPX_FLOAT_lvalue
+DOUBLE = lib.HPX_DOUBLE_lvalue
+POINTER = lib.HPX_POINTER_lvalue
+LONGDOUBLE = lib.HPX_LONGDOUBLE_lvalue
+COMPLEX_FLOAT = lib.HPX_COMPLEX_FLOAT_lvalue
+COMPLEX_DOUBLE = lib.HPX_COMPLEX_DOUBLE_lvalue
+COMPLEX_LONGDOUBLE = lib.HPX_COMPLEX_LONGDOUBLE_lvalue
 
-# Exit the HPX runtime.
-def exit(code):
-    lib.hpx_exit(code)
 
-def run(action_id, *args):
-    arg = ffi.new("int* ", args[0])
-    lib._hpx_run(action_id, 1, arg)
+# Define a dictionary to map argument types to C definition
+_c_def_map = {
+    CHAR: "char",
+    SHORT: "short",
+    USHORT: "unsigned short",
+    SSHORT: "signed short",
+    INT: "int", 
+    UINT: "unsigned int"
+}
 
-def finalize():
-    lib.hpx_finalize()
+
+# Store registered function
+_hpx_action_dict = {}
+
+
+# Helper function to generate a suitable C function from user-defined Python function
+def _generate_hpx_action(user_action, action_arguments):
+    action_arguments_cdef = []
+    for argument in action_arguments:
+        action_arguments_cdef.append(_c_def_map[argument])
+    return ffi.callback("int(" + ",".join(action_arguments_cdef) + ")")(user_action)
 
 
 # Define action types
@@ -61,54 +84,6 @@ COALESCED = lib.HPX_COALESCED
 # Action is a compressed action
 COMPRESSED = lib.HPX_COMPRESSED
 
-# Define argument types
-CHAR = lib.HPX_CHAR_lvalue
-SHORT = lib.HPX_SHORT_lvalue
-USHORT = lib.HPX_USHORT_lvalue
-SSHORT = lib.HPX_SSHORT_lvalue
-INT = lib.HPX_INT_lvalue
-UINT = lib.HPX_UINT_lvalue
-SINT = lib.HPX_SINT_lvalue
-LONG = lib.HPX_LONG_lvalue
-ULONG = lib.HPX_ULONG_lvalue
-SLONG = lib.HPX_SLONG_lvalue
-VOID = lib.HPX_VOID_lvalue
-UINT8 = lib.HPX_UINT8_lvalue
-SINT8 = lib.HPX_SINT8_lvalue
-UINT16 = lib.HPX_UINT16_lvalue
-SINT16 = lib.HPX_SINT16_lvalue
-UINT32 = lib.HPX_UINT32_lvalue
-SINT32 = lib.HPX_SINT32_lvalue
-UINT64 = lib.HPX_UINT64_lvalue
-SINT64 = lib.HPX_SINT64_lvalue
-FLOAT = lib.HPX_FLOAT_lvalue
-DOUBLE = lib.HPX_DOUBLE_lvalue
-POINTER = lib.HPX_POINTER_lvalue
-LONGDOUBLE = lib.HPX_LONGDOUBLE_lvalue
-COMPLEX_FLOAT = lib.HPX_COMPLEX_FLOAT_lvalue
-COMPLEX_DOUBLE = lib.HPX_COMPLEX_DOUBLE_lvalue
-COMPLEX_LONGDOUBLE = lib.HPX_COMPLEX_LONGDOUBLE_lvalue
-
-# Define a dictionary to map argument types to C definition
-_c_def_map = {
-    CHAR: "char",
-    SHORT: "short",
-    USHORT: "unsigned short",
-    SSHORT: "signed short",
-    INT: "int", 
-    UINT: "unsigned int"
-}
-
-
-# Store the C function for each user defined Python function so that they are not garbage collected
-hpx_action_dict = {}
-
-# Helper function to generate a suitable C function from user-defined Python function
-def _generate_hpx_action(user_action, action_arguments):
-    action_arguments_cdef = []
-    for argument in action_arguments:
-        action_arguments_cdef.append(_c_def_map[argument])
-    return ffi.callback("int(" + ",".join(action_arguments_cdef) + ")")(user_action)
 
 # Register an HPX action
 # This must be called prior to hpx_init().
@@ -120,5 +95,45 @@ def register_action(action, action_type, action_attribute, action_arguments):
     hpx_action = _generate_hpx_action(action, action_arguments)
     lib.hpx_register_action(action_type, action_attribute, b'aaa',
                             action_id, len(action_arguments) + 1, hpx_action, *action_arguments)
-    hpx_action_dict[action_id[0]] = hpx_action
+    _hpx_action_dict[action_id] = {'function': hpx_action, 
+                                     'argument_types': action_arguments,
+                                     'argument_lists': []}
     return action_id
+
+
+# Initializes the HPX runtime.
+# This must be called before other HPX functions.
+def init(argv=[]):
+    if len(argv) == 0:
+        c_argc = ffi.NULL
+        c_argv_address = ffi.NULL
+    else:
+        c_argc = ffi.new("int *", len(argv))
+        c_argv = ffi.new("char* []", len(argv))
+        c_argv_obj = [] # store the reference to make sure it is not garbage collected
+        for i in range(len(argv)):
+            c_argv_obj.append(ffi.new("char[]", argv[i].encode('ascii')))
+            c_argv[i] = c_argv_obj[i]
+        c_argv_address = ffi.new("char ***", c_argv)
+    if lib.hpx_init(c_argc, c_argv_address) != lib.HPX_SUCCESS:
+        raise RuntimeError("hpx_init failed")
+
+# Exit the HPX runtime.
+def exit(code):
+    lib.hpx_exit(code)
+
+def run(action_id, *args):
+    function_signature = _hpx_action_dict[action_id]['argument_types']
+    num_arguments = len(function_signature)
+    c_args = []
+    for i in range(num_arguments):
+        c_type = _c_def_map[function_signature[i]] + ' *'
+        c_args.append(ffi.new(c_type, args[i]))
+    lib._hpx_run(action_id, num_arguments, *c_args)
+    _hpx_action_dict[action_id]['argument_lists'].append(c_args)
+
+def finalize():
+    lib.hpx_finalize()
+
+
+
