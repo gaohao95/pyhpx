d50c77e333b2b9309f4493da3a132a147154857e
Hao Gao <gaohao95@gmail.com>
2017-01-24 06:30:31 -0600

    query total number of rank at runtime

--- examples/trapezoid/trapezoid.py
+++ examples/trapezoid/trapezoid.py
@@ -4,9 +4,7 @@ import numpy as np
 
 low = 1.0
 high = 2.0
-num_node = 10
 cell_per_node = 100
-step_size = (high - low) / (num_node * cell_per_node)
 
 def f(x):
     return 3*x**2+2*x+4
@@ -24,15 +22,19 @@ def result_op(array1, array2):
 
 @hpx.create_action()
 def main_action():
+    num_node = hpx.get_num_ranks()
+    step_size = (high - low) / (num_node * cell_per_node)
+    
     result_lco = hpx.Reduce(num_node, (1,), np.dtype(float), result_init, result_op)
     for i in range(num_node):
-        calculate_integral(hpx.THERE(i), low + i*step_size*cell_per_node, result_lco)
+        calculate_integral(hpx.THERE(i), low + i*step_size*cell_per_node,
+            step_size, result_lco)
     print(result_lco.get())
     print(fint(high) - fint(low))
     hpx.exit()
 
 @hpx.create_action()
-def calculate_integral(start, result_lco):
+def calculate_integral(start, step_size, result_lco):
     result = 0
     for i in range(cell_per_node):
         s1 = f(start + i*step_size)
@@ -40,7 +42,6 @@ def calculate_integral(start, result_lco):
         result += (s1 + s2)*step_size/2
     result_lco.set(np.array([result]))
     return hpx.SUCCESS
-
 
 
 if __name__ == '__main__':
