a2799549a96df41aea861119bf6e35ef0ea305fc
Hao Gao <gaohao95@gmail.com>
2017-01-31 10:59:55 -0600

    update trapezoid example

--- examples/trapezoid/trapezoid.py
+++ examples/trapezoid/trapezoid.py
@@ -3,8 +3,8 @@ import sys
 import numpy as np
 
 low = 1.0
-high = 2.0
-cell_per_node = 100
+high = 5000.0
+total_cells = 10000000
 
 def f(x):
     return 3*x**2+2*x+4
@@ -23,18 +23,20 @@ def result_op(array1, array2):
 @hpx.create_action()
 def main_action():
     num_node = hpx.get_num_ranks()
-    step_size = (high - low) / (num_node * cell_per_node)
+    print("program runs on {0} nodes".format(num_node))
+    step_size = (high - low) / total_cells
+    cell_per_node = total_cells // num_node
     
     result_lco = hpx.Reduce(num_node, (1,), np.dtype(float), result_init, result_op)
     for i in range(num_node):
         calculate_integral(hpx.THERE(i), low + i*step_size*cell_per_node,
-            step_size, result_lco)
+            step_size, cell_per_node, result_lco)
     print(result_lco.get())
     print(fint(high) - fint(low))
     hpx.exit()
 
 @hpx.create_action()
-def calculate_integral(start, step_size, result_lco):
+def calculate_integral(start, step_size, cell_per_node, result_lco):
     result = 0
     for i in range(cell_per_node):
         s1 = f(start + i*step_size)
@@ -42,7 +44,6 @@ def calculate_integral(start, step_size, result_lco):
         result += (s1 + s2)*step_size/2
     result_lco.set(np.array([result]))
     return hpx.SUCCESS
-
 
 if __name__ == '__main__':
     hpx.init(sys.argv)
